c883c93602c50da569a17c70febc3746824c7d69
icedmist <talk2icedmist@gmail.com>
2026-05-21 21:18:25 +0100

    feat: add in-memory database helper routines

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.get('/api/slangs', (req, res) => res.json([]));
+app.use(express.static(path.join(__dirname, 'public')));
+
+const getSlangsDb = () => {
+  return JSON.parse(fs.readFileSync(path.join(__dirname, 'slangs.json'), 'utf8'));
+};
+
+app.get('/api/slangs', (req, res) => res.json(getSlangsDb()));
 
 app.listen(PORT, () => console.log('Server running'));
